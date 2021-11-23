f6698572a33c7cc7be7b31a03396160a158871d2
MinhajSadik <MinhajoSadik@gmail.com>
2021-11-23 10:55:12 +0600

    [Done] initial all setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-center">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
